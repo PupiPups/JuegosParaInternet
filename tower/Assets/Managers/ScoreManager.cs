9e1f7c8536a6042904c0c2ebc56f1c9c4371e784
Emanuel <emanuel_318@hotmail.com>
2018-05-05 03:28:30 -0300

    Punto 1 y 2 hecho
    Punto 1 y 2 hechos a mejorar

---
+++ tower/Assets/Managers/ScoreManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class ScoreManager : MonoBehaviour
+{
+    public LeaderboardModel leaderboardModel;
+
+    string leaderboardPath { get { return Path.Combine(RequestManager.baseURL, "leaderboard.php"); } }
+
+    public void SendScore(string name, int score, Action<string> OnError)
+    {
+        WWWForm form = new WWWForm();
+
+        form.AddField("deviceId", SystemInfo.deviceUniqueIdentifier);
+        form.AddField("playerName", name);
+        form.AddField("score", score);
+
+        UnityWebRequest uwr = UnityWebRequest.Post(leaderboardPath, form);
+
+        uwr.chunkedTransfer = false;
+
+        RequestManager.instance.SendRequest(uwr, () =>
+        {
+            Debug.Log(uwr.downloadHandler.text);
+        }, OnError);
+    }
+
+    public void GetLeaderboard(Action<LeaderboardModel> OnSuccess, Action<string> OnError)
+    {
+        RequestManager.instance.DownloadText(leaderboardPath, (string result) =>
+        {
+            JsonUtility.FromJsonOverwrite(result, leaderboardModel);
+            OnSuccess(leaderboardModel);
+        }, OnError);
+    }
+}
---
+++ tower/Assets/UseExample.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UseExample : MonoBehaviour
+{
+    public Text titleText;
+    public Text subTittleText;
+
+    [Space(10)]
+    public string playerName;
+    public int score;
+
+    DataManager dataManager { get { return GetComponent<DataManager>(); } }
+    ScoreManager scoreManager { get { return GetComponent<ScoreManager>(); } }
+
+    void Start()
+    {
+        //le pedimos al data manager que cargue las cosas
+        dataManager.DownloadDataModel(ShowError);
+        //nos suscribimos para saber cuando termina la cosa
+        RequestManager.instance.OnRequestFinished += AssingData;
+    }
+
+    void ShowError(string errorMessage)
+    {
+        Debug.Log(errorMessage);
+    }
+
+    void AssingData()
+    {
+        //una vez que termina, no nos interesa mas la cosa. nos desuscribimos
+        RequestManager.instance.OnRequestFinished -= AssingData;
+
+        //ponemos el texto donde va
+        titleText.text = dataManager.dataModel.gameName;
+        subTittleText.text = dataManager.dataModel.gameDescription;
+    }
+
+    public void SendMyScore()
+    {
+        //enviamos el score
+        scoreManager.SendScore(playerName, score, ShowError);
+    }
+
+    public void GetScores()
+    {
+        //pedimos los scores, a la vuelta la respuesta la maneja ShowScores
+        scoreManager.GetLeaderboard(ShowScores, ShowError);
+    }
+
+    public void ShowScores(LeaderboardModel model)
+    {
+        //lo mostramos en la UI
+        //implementacion libre :D
+    }
+}
