c50191123f05b94f13db5cc9961f3b4ae3884af4
Emanuel <emanuel_318@hotmail.com>
2018-05-06 04:05:32 -0300

    Disparos y animaciones
    se añadio el disparo y las animaciones de forma precaria

---
+++ tower/Assets/Game Scripts/Bala_scr.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bala_scr : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GetComponent<Rigidbody2D>().AddForce(new Vector2(1, 0), ForceMode2D.Impulse);
+    }
+}
---
+++ tower/Assets/Game Scripts/Player_scr.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_scr : MonoBehaviour {
+    public GameObject bala;
+    public Vector2 inicio;
+    // Use this for initialization
+    void Start () {
+        inicio.x = -6f;
+        inicio.y = 0.7f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("space"))
+        {
+            Instantiate(bala, inicio, Quaternion.identity);
+
+        }
+		
+	}
+}
