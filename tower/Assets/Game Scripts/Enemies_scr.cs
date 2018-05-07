60a4d1524d423043d7a64ba997ce238bf100b14d
S3rMaX <sergio92diaz@gmail.com>
2018-05-06 23:30:27 -0300

    Script Enemigo
    Creeacion del Script del enemigo con la condicion de al llegar a (0) muere.

---
+++ tower/Assets/Game Scripts/Enemies_scr.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemies_scr : MonoBehaviour
+{
+    public string name;
+    public int health;
+    public int damage;
+    public int velocity;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if (health == 0)
+        {
+            DestroyObject(this.gameObject);
+        } 
+	}
+}
