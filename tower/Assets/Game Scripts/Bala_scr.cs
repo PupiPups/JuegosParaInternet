9d5fc879022bf582d5332eb77e587d18934c9adc
S3rMaX <sergio92diaz@gmail.com>
2018-05-07 03:20:08 -0300

    Cosas de Cheecho

--- tower/Assets/Game Scripts/Bala_scr.cs
+++ tower/Assets/Game Scripts/Bala_scr.cs
@@ -3,14 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Bala_scr : MonoBehaviour {
+    
 
-	// Use this for initialization
-	void Start () {
-		
+    // Use this for initialization
+    public void RomperBala ()
+    {
+        Destroy(this);
 	}
 	
 	// Update is called once per frame
 	void Update () {
         GetComponent<Rigidbody2D>().AddForce(new Vector2(1, 0), ForceMode2D.Impulse);
     }
+
+    
 }
--- tower/Assets/Game Scripts/Enemies_scr.cs
+++ tower/Assets/Game Scripts/Enemies_scr.cs
@@ -9,6 +9,9 @@ public class Enemies_scr : MonoBehaviour
     public int damage;
     public float speed;
 
+   Rigidbody2D bala;
+    
+
 
     // Use this for initialization
     void Start()
@@ -27,14 +30,21 @@ public class Enemies_scr : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (health != 0)
-        {
-            health -= 5;
-        }
+        //if (collision.gameObject.CompareTag("Bala"))
+        //{
+            if (health != 0)
+            {
+                health -= 5;
+                DestroyObject(collision.gameObject);
 
-        if (health == 0)
-        {
-            DestroyObject(this.gameObject);
-        }
+            }
+
+            if (health == 0)
+            {
+                DestroyObject(collision.gameObject);
+                DestroyObject(this.gameObject);
+
+            }
+        //}        
     }
 }
