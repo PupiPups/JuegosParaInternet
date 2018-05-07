9ba759f47db3081925497481aa3cbbd45a5a8459
PupiPups <emanuel_318@hotmail.com>
2018-05-07 02:29:06 -0300

    papito arreglo al doom

--- tower/Assets/Game Scripts/Eventos_menu_scr.cs
+++ tower/Assets/Game Scripts/Eventos_menu_scr.cs
@@ -19,8 +19,34 @@ public class Eventos_menu_scr : MonoBehaviour {
 	// Update is called once per frame
 	public void Sel_Niv (Dropdown dif) {
 		PlayerPrefs.SetInt ("Dificul", dif.value);
-		print (dif);
-		SceneManager.LoadScene (2);
+		if (PlayerPrefs.GetInt ("Dificul") == 0) {
+			PlayerPrefs.SetInt ("Enemigos", 3);
+			PlayerPrefs.SetInt ("Jefes", 0);
+			PlayerPrefs.SetString ("Chaos", "false");
+			SceneManager.LoadScene (2);
+
+		} else if (PlayerPrefs.GetInt ("Dificul") == 1) {
+			PlayerPrefs.SetInt ("Enemigos", 5);
+			PlayerPrefs.SetInt ("Jefes", 1);
+			PlayerPrefs.SetString ("Chaos", "false");
+			SceneManager.LoadScene (2);
+		} else if (PlayerPrefs.GetInt ("Dificul") == 2) {
+			PlayerPrefs.SetInt ("Enemigos", 10);
+			PlayerPrefs.SetInt ("Jefes", 1);
+			PlayerPrefs.SetString ("Chaos", "false");
+			SceneManager.LoadScene (2);
+		} else if (PlayerPrefs.GetInt ("Dificul") == 3) {
+			PlayerPrefs.SetInt ("Enemigos", 20);
+			PlayerPrefs.SetInt ("Jefes", 2);
+			PlayerPrefs.SetString ("Chaos", "true");
+			SceneManager.LoadScene (2);
+		}
+		else if (PlayerPrefs.GetInt ("Dificul") == 4) {
+			PlayerPrefs.SetInt ("Enemigos", 50);
+			PlayerPrefs.SetInt ("Jefes", 10);
+			PlayerPrefs.SetString ("Chaos", "true");
+			SceneManager.LoadScene (2);
+		}
 	}
 
 
