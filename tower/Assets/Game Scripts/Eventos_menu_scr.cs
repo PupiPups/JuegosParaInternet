0f21c8f65ea54cd60b67db77933864367e558f49
PupiPups <emanuel_318@hotmail.com>
2018-05-06 21:27:17 -0300

    Algo raro con la notebook
    commit raro desde la notebook

---
+++ tower/Assets/Game Scripts/Eventos_menu_scr.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Eventos_menu_scr : MonoBehaviour {
+
+	// Use this for initialization
+	public void Sel_Dif () {
+		SceneManager.LoadScene (1);
+	}
+	
+	// Update is called once per frame
+	public void Sel_Niv () {
+		SceneManager.LoadScene (2);
+	}
+}
